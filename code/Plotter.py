8dab2ca2b52c883a5f23358f0cedf65647bba611
José Vines <jose.vines@ug.uchile.cl>
2019-08-13 17:31:46 -0400

    Main plot works sort of.

--- code/Plotter.py
+++ code/Plotter.py
@@ -7,10 +7,14 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import scipy as sp
 from matplotlib import rcParams
+from matplotlib.gridspec import GridSpec
 
 from phot_utils import *
 from sed_library import *
 from Star import *
+
+with open('interpolations.pkl', 'rb') as intp:
+    interpolators = pickle.load(intp)
 
 
 class SEDPlotter:
@@ -24,13 +28,12 @@ class SEDPlotter:
         self.read_config()
 
         self.model = model_grid(
-            self.theta, self.fitter.star, self.fitter.interpolators)
+            self.theta, self.fitter.star, interpolators)
 
     def __extract_params(self):
         # teff, logg, z, dist, rad, Av = theta
-        theta = []
-        for i in range(self.fitter.ndim):
-            theta.append(sp.median(self.fitter.chain[:, i]))
+        theta = self.fitter.sampler.get_chain(flat=True)[sp.argmax(
+            self.fitter.sampler.get_log_prob(flat=True))]
         self.theta = theta
 
     def __extract_info(self):
@@ -57,40 +60,83 @@ class SEDPlotter:
         """Create the plot of the SED."""
         # TODO: Finish the function
 
+        # Get plot ylims.
         ymin = (self.flux * self.wave).min()
         ymax = (self.flux * self.wave).max()
 
+        # Calculate model
         mod_f = []
         for k in self.model.keys():
             mod_f.append(self.model[k])
         mod_f = sp.array(mod_f)
 
-        f, ax = plt.subplots(figsize=self.figsize)
+        # Get models residuals
+        residuals, errors = get_residuals(
+            self.theta, self.fitter.star, interpolators)
 
+        # resdiuals = residuals / errors
+        norm_res = residuals / errors
+
+        # Create plot layout
+
+        f = plt.figure(figsize=self.figsize)
+        gs = GridSpec(2, 1, height_ratios=[3, 0.5], hspace=0)
+
+        ax = f.add_subplot(gs[0])
+        ax_r = f.add_subplot(gs[1])
+
+        # Model plot
         ax.errorbar(self.wave, self.flux * self.wave,
                     xerr=self.bandpass, yerr=self.flux_er,
                     fmt=',',
                     # ecolor=self.error_color,
                     # color='turquoise',
-                    marker=None,
-                    alpha=self.error_alpha)
+                    marker=None)
 
         ax.scatter(self.wave, self.flux * self.wave,
                    edgecolors=self.edgecolors,
                    c=self.marker_colors,
                    s=self.scatter_size,
-                   alpha=self.scatter_linewidths)
+                   alpha=self.scatter_alpha)
 
         ax.scatter(self.wave, mod_f * self.wave,
                    marker='D',
-                   edgecolors=self.edgecolors,
-                   c=self.marker_colors_model,
+                   edgecolors=self.marker_colors_model,
                    s=self.scatter_size,
-                   alpha=self.scatter_linewidths)
+                   facecolor='',
+                   lw=3)
 
+        # Residual plot
+        ax_r.axhline(y=0, lw=2, ls='--', c='k')
+
+        ax_r.errorbar(self.wave, self.flux * self.wave / errors,
+                      xerr=self.bandpass, yerr=self.flux_er,
+                      fmt=',',
+                      # ecolor=self.error_color,
+                      # color='turquoise',
+                      marker=None)
+        ax_r.scatter(self.wave, self.flux * self.wave / errors,
+                     edgecolors=self.edgecolors,
+                     c=self.marker_colors,
+                     s=self.scatter_size,
+                     alpha=self.scatter_alpha)
+        ax_r.scatter(self.wave, norm_res,
+                     marker='D',
+                     edgecolors=self.marker_colors_model,
+                     s=self.scatter_size,
+                     facecolor='',
+                     lw=3,
+                     zorder=10)
+
+        # Formatting
         ax.set_ylim([ymin * .8, ymax * 1.1])
+        xmin, xmax = ax.get_xlim()
+        print(xmin)
+        print(xmax)
+        # ax_r.set_xlim([1, xmax])
         ax.set_xscale('log', nonposx='clip')
         ax.set_yscale('log', nonposy='clip')
+        ax_r.set_xscale('log', nonposx='clip')
         ax.set_xlabel(r'$\lambda (\mu m)$',
                       fontsize=self.fontsize,
                       fontname=self.fontname
@@ -99,17 +145,34 @@ class SEDPlotter:
                       fontsize=self.fontsize,
                       fontname=self.fontname
                       )
-        ax.set_xticks(range(1, 11))
-        ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
+        ax_r.set_xticks(range(1, 11))
+        ax_r.get_xaxis().set_major_formatter(ticker.ScalarFormatter())
         ax.tick_params(
             axis='both', which='major',
             labelsize=self.tick_labelsize
         )
+        ax_r.tick_params(
+            axis='both', which='major',
+            labelsize=self.tick_labelsize
+        )
+
+        labels = [item.get_text() for item in ax.get_xticklabels()]
+
+        empty_string_labels = [''] * len(labels)
+        ax.set_xticklabels(empty_string_labels)
+
         for tick in ax.get_yticklabels():
             tick.set_fontname(self.fontname)
-        for tick in ax.get_xticklabels():
+        for tick in ax_r.get_yticklabels():
+            tick.set_fontname(self.fontname)
+        for tick in ax_r.get_xticklabels():
             tick.set_fontname(self.fontname)
 
+        plt.savefig('test.png', bbox_inches='tight')
+        pass
+
+    def plot_chains(self):
+        """Plot SED chains."""
         pass
 
     def read_config(self):
@@ -118,8 +181,8 @@ class SEDPlotter:
         # SCATTER
         self.scatter_size = 80
         self.edgecolors = 'k'
-        self.marker_colors = 'cyan'
-        self.marker_colors_model = 'magenta'
+        self.marker_colors = 'deepskyblue'
+        self.marker_colors_model = 'mediumvioletred'
         self.scatter_alpha = .85
         self.scatter_linewidths = 1
 
