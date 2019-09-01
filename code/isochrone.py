b197ad58b6e2891314b065e421d0008ea914fd48
José Vines <jose.vines@ug.uchile.cl>
2019-09-01 15:24:03 -0400

    Added isochrone.py. It estimates logg using MIST isochrones.

---
+++ code/isochrone.py
@@ -0,0 +1,18 @@
+"""Estimate logg using MIST isochrones."""
+
+import os
+
+from isochrones import SingleStarModel, get_ichrone
+
+from sed_library import credibility_interval
+
+
+def estimate(bands, params):
+    """Estimate logg using MIST isochrones."""
+    mist = get_ichrone('mist', bands=bands)
+    model = SingleStarModel(mist, **params)
+    model.fit(resume=False, verbose=False)
+    logg_samples = model.derived_samples['logg']
+    med, lo, up = credibility_interval(logg_samples)
+    med_e = 3 * max([med - lo, up - med])
+    return med, med_e
