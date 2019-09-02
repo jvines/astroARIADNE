023492787875e518d3eaddfbd8b767ec7e06d981
José Vines <jose.vines@ug.uchile.cl>
2019-09-01 23:54:24 -0400

    Changed isochrones live points.

--- code/isochrone.py
+++ code/isochrone.py
@@ -11,7 +11,7 @@ def estimate(bands, params):
     """Estimate logg using MIST isochrones."""
     mist = get_ichrone('mist', bands=bands)
     model = SingleStarModel(mist, **params)
-    model.fit(resume=False, verbose=False)
+    model.fit(resume=False, verbose=False, n_live_points=500)
     logg_samples = model.derived_samples['logg']
     med, lo, up = credibility_interval(logg_samples)
     med_e = 2 * max([med - lo, up - med])
