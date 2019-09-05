935207246dc9e74c24e2ed84404c2e2659a44a71
José Vines <jose.vines@ug.uchile.cl>
2019-09-05 15:35:39 -0400

    Added a distance prior to mist isochrones so good logg values are found

--- code/isochrone.py
+++ code/isochrone.py
@@ -3,6 +3,7 @@
 import os
 
 from isochrones import SingleStarModel, get_ichrone
+from isochrones.priors import GaussianPrior
 
 from utils import credibility_interval
 
@@ -11,6 +12,9 @@ def estimate(bands, params):
     """Estimate logg using MIST isochrones."""
     mist = get_ichrone('mist', bands=bands)
     model = SingleStarModel(mist, **params)
+    dist = 1 / (params['parallax'][0] * 0.001)
+    dist_e = dist * params['parallax'][1] / params['parallax'][0]
+    model._priors['distance'] = GaussianPrior(dist, 3 * dist_e)
     model.fit(resume=False, verbose=False, n_live_points=500)
     logg_samples = model.derived_samples['logg']
     med, lo, up = credibility_interval(logg_samples)
