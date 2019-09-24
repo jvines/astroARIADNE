09602ef0dcba675cec63c38cf154da375337a37d
José Vines <jose.vines@ug.uchile.cl>
2019-09-24 12:52:43 -0300

    Added PriorError for handling prior specific errors.

--- code/Error.py
+++ code/Error.py
@@ -38,3 +38,29 @@ class InstanceError(Error):
         self.exp_inst = exp_inst
         self.message = 'Input object is an instance of ' + in_inst.__class__
         self.message += ' expected class is ' + exp_inst
+
+
+class PriorError(Error):
+    """Exception raised if problem arises with priors.
+
+    Attributes
+    ----------
+    par -- parameter
+    type -- error type, can be 0, 1
+    message -- explanation of the error
+
+    Notes
+    -----
+    Type 0 error means the parameter isn't recognized.
+    Type 1 occurs if norm parameter is found alongside dist or rad.
+    Type 2 occurs if the selected prior is illegal.
+    """
+
+    def __init__(self, par, type):
+        self.message = 'Parameter ' + par
+        if type == 0:
+            self.message += " isn't recognized. The allowed parameters are: "
+            self.message += 'teff, logg, z, dist, rad, norm, Av, inflation'
+        if type == 1:
+            self.message += ' has been found alongside norm. Parameters rad '
+            self.message += 'and dist are incompatible with norm.'
