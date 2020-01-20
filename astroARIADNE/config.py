45d2ced214441d294b5f3edc0792a47c26b16a03
José Vines <jose.vines@ug.uchile.cl>
2020-01-20 14:53:47 -0300

    Small change to config file

--- astroARIADNE/config.py
+++ astroARIADNE/config.py
@@ -1,12 +1,14 @@
 """Some configs for the code to run."""
-import os
 import inspect
+import os
 
 from pkg_resources import resource_filename
 
+__ROOT__ = '/'.join(os.path.abspath(inspect.getfile(inspect.currentframe())
+                                    ).split('/')[:-1])
 gridsdir = resource_filename('astroARIADNE', 'Datafiles/model_grids')
 priorsdir = resource_filename('astroARIADNE', 'Datafiles/prior')
-filesdir = resource_filename('astroARIADNE', 'Datafiles/')
+filesdir = resource_filename('astroARIADNE', 'Datafiles')
 
 try:
     modelsdir = os.environ['ARIADNE_MODELS']
