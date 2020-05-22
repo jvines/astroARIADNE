d06d5132311228ab3952c73af11e9cd9540f5863
José Vines <jose.vines@ug.uchile.cl>
2020-05-22 19:12:07 -0400

    Limit metallicity on isochrones to 0.5

--- astroARIADNE/isochrone.py
+++ astroARIADNE/isochrone.py
@@ -45,6 +45,8 @@ def estimate(bands, params, logg=True):
         return sp.zeros(10), sp.zeros(10)
     if 'feh' in params.keys():
         fe, fe_e = params['feh']
+        if fe > 0.5:
+            fe = 0.5
         model._priors['feh'] = GaussianPrior(fe, fe_e)
     if 'mass' in params.keys():
         m, m_e = params['mass']
