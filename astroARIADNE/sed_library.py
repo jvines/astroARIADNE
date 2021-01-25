56b084fd920b72f2a3beb3a38f9a021d57bf5455
José Vines <jose.vines@ug.uchile.cl>
2021-01-25 18:48:59 -0300

    Small changes. loglike is now written with numba

--- astroARIADNE/sed_library.py
+++ astroARIADNE/sed_library.py
@@ -1,23 +1,18 @@
 """sed_library contain the model, prior and likelihood to be used."""
 
-import pickle
-from contextlib import closing
-
-import astropy.units as u
+import numba as nb
 import numpy as np
 from extinction import apply
 
-from .phot_utils import *
 from .utils import get_noise_name
 
 
 def build_params(theta, flux, flux_e, filts, coordinator, fixed, use_norm):
     """Build the parameter vector that goes into the model."""
+    params = np.zeros(len(coordinator))
     if use_norm:
-        params = np.zeros(len(coordinator))
         order = np.array(['teff', 'logg', 'z', 'norm', 'Av'])
     else:
-        params = np.zeros(len(coordinator))
         order = np.array(
             ['teff', 'logg', 'z', 'dist', 'rad', 'Av']
         )
@@ -57,8 +52,7 @@ def get_interpolated_flux(temp, logg, z, filts, interpolators):
 
     """
     values = (logg, temp, z)
-    vals = interpolators(values, filts)
-    flux = vals
+    flux = interpolators(values, filts)
     return flux
 
 
@@ -69,13 +63,10 @@ def model_grid(theta, filts, wave, interpolators, use_norm, av_law):
     -----------
     theta : array_like
         The parameters of the fit: teff, logg, z, radius, distance
-
     star : Star
         The Star object containing all relevant information regarding the star.
-
     interpolators : dict
         A dictionary with the interpolated grid.
-
     use_norm : bool
         False for a full fit  (including radius and distance). True to fit
         for a normalization constant instead.
@@ -94,7 +85,8 @@ def model_grid(theta, filts, wave, interpolators, use_norm, av_law):
         teff, logg, z, norm, Av = theta[:5]
     else:
         teff, logg, z, dist, rad, Av = theta[:6]
-        dist = (dist * u.pc).to(u.solRad).value
+        # dist = (dist * u.pc).to(u.solRad).value
+        dist *= 4.435e+7  # Transform from pc to solRad
 
     flux = get_interpolated_flux(teff, logg, z, filts, interpolators)
 
@@ -114,7 +106,7 @@ def get_residuals(theta, flux, flux_er, wave, filts, interpolators, use_norm,
     start = 5 if use_norm else 6
     inflation = theta[start:]
     residuals = model - flux
-    errs = np.sqrt(flux_er ** 2 + inflation**2)
+    errs = np.sqrt(flux_er ** 2 + inflation ** 2)
     return residuals, errs
 
 
@@ -124,13 +116,22 @@ def log_likelihood(theta, flux, flux_er, wave, filts, interpolators, use_norm,
     res, ers = get_residuals(theta, flux, flux_er, wave,
                              filts, interpolators, use_norm, av_law)
 
-    c = np.log(2 * np.pi * ers ** 2)
-    lnl = (c + (res ** 2 / ers ** 2)).sum()
+    # c = np.log(2 * np.pi * ers ** 2)
+    # lnl = (c + (res ** 2 / ers ** 2)).sum()
+    lnl = fast_loglik(res, ers)
 
     if not np.isfinite(lnl):
         return -1e300
 
     return -.5 * lnl
+
+
+@nb.njit
+def fast_loglik(res, ers):
+    ers2 = ers ** 2
+    c = np.log(2 * np.pi * ers2)
+    lnl = (c + (res ** 2 / ers2)).sum()
+    return lnl
 
 
 def prior_transform_dynesty(u, flux, flux_er, filts, prior_dict, coordinator,
