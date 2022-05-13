cbe259daaa7f50ccc197b6fe9cb29bda79961fe9
Jose Vines <jose.vines@ug.uchile.cl>
2022-05-13 16:25:32 -0400

    Added more information to the init

--- astroARIADNE/__init__.py
+++ astroARIADNE/__init__.py
@@ -8,8 +8,20 @@ parameters posteriors as in the Bayesian Model Average framework. This
 averages over the models and thus the averaged posteriors account for model
 specific uncertainties.
 """
-from . import utils
+
+from .config import __bibtex__
+__author__ = 'Jose Ignacio Vines'
+__email__ = 'jose.vines@ug.uchile.cl'
+__license__ = 'MIT'
+__description__ = 'Bayesian Model Averaging SED fitter'
+__version__ = '0.5.5'
+
 from .fitter import Fitter
 from .star import Star
-from .librarian import Librarian
 from .plotter import SEDPlotter
+
+__all__ = [
+    'Fitter',
+    'Star',
+    'SEDPlotter'
+]
